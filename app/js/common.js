fe79802b1a454f15b4d693d3bedf2f39838d4ac0
letehaha <letehaha@gmail.com>
2016-06-08 21:09:28 +0300

    update

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,11 @@
 $(function() {
 
+	$('.barra-nivel').each(function() {
+	  var valorLargura = $(this).data('nivel');
+	  var valorNivel = $(this).html("<span class='valor-nivel'>"+valorLargura+"</span>");
+	    $(this).animate({
+	        width: valorLargura
+	    });
+	});
+
 });
